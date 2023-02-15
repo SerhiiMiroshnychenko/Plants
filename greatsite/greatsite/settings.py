9a1220a6dba7923fd4252e0c40f4074089078654
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-02-15 16:48:03 +0200

    Plants

--- greatsite/greatsite/settings.py
+++ greatsite/greatsite/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'  # префікс URL адрес для статичних файлів
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # шлях до загальної директорії зі статичними
+# файлами для реального веб-серверу
+STATICFILES_DIRS = []  # Список нестандартних шляхів до статичних файлів
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
